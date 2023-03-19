9ba5ace28f7be877eb69f62c071e2e835c0f2b83
DaniloJevtovic <danilo_jevtovic@hotmail.com>
2023-03-19 18:05:42 +0100

    male izmjene, promjenjeni portovi

--- take-quiz/src/main/java/com/quiz/takequiz/TakeQuizApplication.java
+++ take-quiz/src/main/java/com/quiz/takequiz/TakeQuizApplication.java
@@ -3,7 +3,7 @@ package com.quiz.takequiz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.quiz.takequiz", "com.quiz.rabbitmq"})
 public class TakeQuizApplication {
     public static void main(String[] args) {
         SpringApplication.run(TakeQuizApplication.class, args);
--- user/src/main/java/com/quiz/user/user/User.java
+++ user/src/main/java/com/quiz/user/user/User.java
@@ -19,14 +19,19 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String name;
     private String email;
+
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
+
     @Enumerated(EnumType.STRING)
     private Role role;
+
     @Enumerated(EnumType.STRING)
     private UserStatus status;
+
     @JsonFormat(pattern = "dd.MM.yyyy / HH:mm:ss", timezone = "Europe/Belgrade")
     private LocalDateTime registrationDate;
 }
