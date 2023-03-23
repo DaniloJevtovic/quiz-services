744f59e0bf2e89dcc0ea93ffbb60a21b6dfa497b
DaniloJevtovic <danilo_jevtovic@hotmail.com>
2023-03-23 19:11:22 +0100

    brisanje ponudjenih odgovora za pitanje

--- question/src/main/java/com/quiz/question/QuestionService.java
+++ question/src/main/java/com/quiz/question/QuestionService.java
@@ -1,5 +1,6 @@
 package com.quiz.question;
 
+import com.quiz.rabbitmq.RabbitMQMessageProducer;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -14,6 +15,7 @@ import java.util.List;
 public class QuestionService {
 
     private final QuestionRepository questionRepository;
+    private final RabbitMQMessageProducer rabbitMQMessageProducer;
 
     public Page<Question> getAllQuestions(Pageable pageable) {
         return questionRepository.findAll(pageable);
@@ -32,18 +34,18 @@ public class QuestionService {
     }
 
     public Question updateQuestion(Question question) {
-        // provjeriti da li je kviz vec radjen
+        // provjeriti da li je kviz vec radjen - to moze na frontend-u
         return saveQuestion(question);
     }
 
     public void deleteQuestion(String id) {
-        // provjera da li je kviz vec radjen
         // brisanje svih ponudjenih odgovora za pitanje
+        rabbitMQMessageProducer.publish(id, "del-question.exchange", "del-question.routing-key");
         questionRepository.deleteById(id);
     }
 
     public Long deleteAllQuestionsForQuiz(Integer quizId) {
-        log.info("obrisane sva pitanja za kviz");
+        log.info("obrisana sva pitanja za kviz");
         return questionRepository.deleteByQuizId(quizId);
     }
 }
