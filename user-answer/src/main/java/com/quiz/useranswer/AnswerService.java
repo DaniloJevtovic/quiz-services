510c164a158bed5bb75bac7dd5d71c11b127333b
DaniloJevtovic <danilo_jevtovic@hotmail.com>
2023-03-30 06:52:27 +0200

    provjere selektovanih odgovora, racunanje rezultata za kviz..

--- user-answer/src/main/java/com/quiz/useranswer/AnswerService.java
+++ user-answer/src/main/java/com/quiz/useranswer/AnswerService.java
@@ -1,6 +1,10 @@
 package com.quiz.useranswer;
 
+import com.quiz.clients.choice.ChoiceClient;
+import com.quiz.rabbitmq.RabbitMQMessageProducer;
+import com.quiz.clients.result.UpdatePointsDTO;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -9,9 +13,12 @@ import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class AnswerService {
 
     private final AnswerRepository answerRepository;
+    private final RabbitMQMessageProducer rabbitMQMessageProducer;
+    private final ChoiceClient choiceClient;
 
     public Page<Answer> getAll(Pageable pageable) {
         return answerRepository.findAll(pageable);
@@ -21,12 +28,8 @@ public class AnswerService {
         return answerRepository.findById(id).orElse(null);
     }
 
-    public List<Answer> getAnswersForQuestionAndSolver(String questionId, Integer solverId) {
-        return answerRepository.findByQuestionIdAndSolverId(questionId, solverId);
-    }
-
-    public List<Answer> getAnswersForQuestionSolverAndType(String questionId, Integer solverId, AnswerType type) {
-        return answerRepository.findByQuestionIdAndSolverIdAndType(questionId, solverId, type);
+    public List<Answer> getAnswersForResultAndQuestion(Integer resId, String questionId) {
+        return answerRepository.findByQuizResultIdAndQuestionId(resId, questionId);
     }
 
     public List<Answer> getAnswersForResult(Integer resultId) {
@@ -38,27 +41,64 @@ public class AnswerService {
     }
 
     // metoda racuna ukupan rezultat za citav kviz (rezultat)
-    // moze kad se pritisne dugme finish ili istekne vrijeme
     public Double calculateScoreForSolvedQuiz(Integer resultId) {
-        // pozvati servis za update rezultata
+        checkChosenAnswersForResult(resultId);  // provjera selektovanih odgovora
         return getAnswersForResult(resultId).stream().mapToDouble(a -> a.getPoints()).sum();
     }
 
     // racuna rezultat za pitanje
-    public Double calculateScoreForQuestion(String questionId, Integer solverId) {
-        return getAnswersForQuestionAndSolver(questionId, solverId)
+    public Double calculateScoreForQuestion(Integer resId, String questionId) {
+        return getAnswersForResultAndQuestion(resId, questionId)
                 .stream().mapToDouble(ans -> ans.getPoints()).sum();
     }
 
-    // vlasnik kviza oznacava da li je odgovor tacan ili netacan
+    // vlasnik kviza oznacava da li je odgovor tacan ili netacan - rucno
     public Answer setCorrect(SetAnsCorrectDTO dto) {
         Answer answer = getAnswerById(dto.answerId());
         answer.setCorrect(dto.correct());
         answer.setPoints(dto.points());
+
+        // poziv servisa za update rezultata
+        rabbitMQMessageProducer.publish(
+                new UpdatePointsDTO(answer.getQuizResultId(), dto.points()),
+                "results.exchange",
+                "results.routing-key"
+        );
+
         return answerRepository.save(answer);
+    }
+
+    // lista odgovora za rezultat po tipu
+    public List<Answer> getAnswersForSolvedQuizByType(Integer resId, AnswerType type) {
+        return answerRepository.findByQuizResultIdAndType(resId, type);
+    }
+
+    // provjerara selektovanih odgovora (po zavrsetku kviza!)
+    public Double checkChosenAnswersForResult(Integer resId) {
+        List<Answer> chosenAnswers = getAnswersForSolvedQuizByType(resId, AnswerType.CHOICE);
+        double score = 0.0;
+
+        // proci kroz sve IZABRANE odgovore i provjeriti da li su tacni
+        for (Answer ans : chosenAnswers) {
+            // poziv choice servisa za provjeru tacnosti odgovora
+            ans.setCorrect(choiceClient.isCorrect(ans.getAnswer()));
+
+            // za sad samo pozitivni poeni
+            if (ans.getCorrect()) score += ans.getPoints();
+            else ans.setPoints(0.0);    // ako nije tacan postavi poene na 0
+
+            // update sacuvanog odgovora (tacno/netacno)
+            answerRepository.save(ans);
+        }
+
+        return score;
     }
 
     public void deleteAnswer(String answerId) {
         answerRepository.deleteById(answerId);
+    }
+
+    public void deleteAllAnswers() {
+        answerRepository.deleteAll();
     }
 }
