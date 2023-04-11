3674aff622862583d02a6def5e41a2a8fc8385f2
DaniloJevtovic <danilo_jevtovic@hotmail.com>
2023-04-11 06:37:40 +0200

    male promjene u kontroleru za autentifikaciju

--- user/src/main/java/com/quiz/user/controler/AuthController.java
+++ user/src/main/java/com/quiz/user/controler/AuthController.java
@@ -2,9 +2,11 @@ package com.quiz.user.controler;
 
 import com.quiz.user.dto.LoginDTO;
 import com.quiz.user.dto.RegisterDTO;
+import com.quiz.user.security.JwtUtil;
 import com.quiz.user.user.User;
 import com.quiz.user.user.UserService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -16,18 +18,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/auth")
 @AllArgsConstructor
+@Slf4j
 public class AuthController {
 
     private final UserService userService;
+    private final JwtUtil jwtUtil;
 
     @PostMapping("/register")
-    public User registerUser(@RequestBody RegisterDTO dto) {
-        return userService.saveUser(dto);
+    public ResponseEntity<?> registerUser(@RequestBody RegisterDTO dto) {
+        if (userService.saveUser(dto) == null)
+            return ResponseEntity.badRequest().body("Korisnik sa tom adresom vec postoji");
+
+        return ResponseEntity.ok("Korisnik uspjesno registrovan");
     }
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginDTO loginDTO) {
-        return new ResponseEntity<>("neispranvi kredencijali",HttpStatus.BAD_REQUEST);
+        if (userService.checkCredentials(loginDTO))
+            return new ResponseEntity<>(jwtUtil.generateToken(loginDTO.email()), HttpStatus.OK);
+
+        return new ResponseEntity<>("neispranvi kredencijali", HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping("/validateToken")
+    public Boolean validateToken(@RequestBody String token) {
+        log.info("validacija...");
+        return jwtUtil.validateToken(token);
     }
 
 }
